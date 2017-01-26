3bc87a9b77e0ac9aea5229fcf0b43098a82c8678
Charlie Person <charlie.person@hyperisland.se>
2017-01-25 21:37:22 -0600

    Completes sequence test

--- BeatBox/src/com/charlieperson/MusicTest1.java
+++ BeatBox/src/com/charlieperson/MusicTest1.java
@@ -5,7 +5,12 @@ import javax.sound.midi.*;
 public class MusicTest1 {
     public void play() {
         try {
-            Sequencer sequencer = MidiSystem.getSequencer();
+            Sequencer player = MidiSystem.getSequencer();
+            Sequence seq = new Sequence(timing, 4);
+            Track track = seq.createTrack();
+            track.add(myMidiEvent1);
+            player.setSequence(seq);
+            player.start();
             System.out.println("We got a sequencer");
         } catch(MidiUnavailableException ex) {
             System.out.println("Bummer....");
